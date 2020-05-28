8c7aeda97d3ca7d345a50e7b35676c2fac0093b1
Zhengfa Dang <zfdang@users.noreply.github.com>
2020-05-28 10:07:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,4 +10,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 
 
 #2. Customize revision
-sed -i "/echo \"DISTRIB_REVISION/ s/'\"/ (Compile by zfdang on $(date '+%Y-%m-%d'))'\"/" openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/echo \"DISTRIB_REVISION/ s/'\"/ (Compiled by zfdang on $(date '+%Y-%m-%d'))'\"/" openwrt/package/lean/default-settings/files/zzz-default-settings
