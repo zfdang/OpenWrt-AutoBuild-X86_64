25c127d3f8c6dd6f9c929589d9351aa6e3dceaf8
Zhengfa Dang <zfdang@users.noreply.github.com>
2020-05-28 10:06:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,7 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+
+
+#2. Customize revision
+sed -i "/echo \"DISTRIB_REVISION/ s/'\"/ (Compile by zfdang on $(date '+%Y-%m-%d'))'\"/" openwrt/package/lean/default-settings/files/zzz-default-settings
