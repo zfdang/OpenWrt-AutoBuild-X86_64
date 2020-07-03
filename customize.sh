3976f93fd7ff3b5b8c8528ee087cae96dd424a03
Zhengfa Dang <zfdang@users.noreply.github.com>
2020-07-03 19:18:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,6 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> openwrt/feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> openwrt/feeds.conf.default
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> openwrt/feeds.conf.default
+cat openwrt/feeds.conf.default
 
 # 3. Customize revision
 sed -i "/echo \"DISTRIB_REVISION/ s/'\"/ (Compiled by zfdang on $(date '+%Y-%m-%d'))'\"/" openwrt/package/lean/default-settings/files/zzz-default-settings
