1d05364dee3cacac43c57f7615a5a8d069faece6
Zhengfa Dang <zfdang@users.noreply.github.com>
2020-07-03 19:04:16 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,10 +5,14 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+# 1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
+# 2. add additional feeds
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> openwrt/feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> openwrt/feeds.conf.default
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> openwrt/feeds.conf.default
 
-#2. Customize revision
+# 3. Customize revision
 sed -i "/echo \"DISTRIB_REVISION/ s/'\"/ (Compiled by zfdang on $(date '+%Y-%m-%d'))'\"/" openwrt/package/lean/default-settings/files/zzz-default-settings
 cat openwrt/package/lean/default-settings/files/zzz-default-settings | grep DISTRIB_REVISION
